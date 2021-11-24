cf8196e8c3af3d40592b454154feb555ba85bb6a
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-11-24 11:56:30 +0100

    B4.1.2 b: Projektmappe "Kaffeeautomat" erstellt und Ausgabe des Menüs programmiert

---
+++ Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Herzlich Willkommen beim Kaffe-Automaten!" << endl
+		<< "Bitte wählen Sie aus:" << endl
+		<< "(k) Kaffee" << endl
+		<< "(e) Espresso" << endl
+		<< "(s) Service-Mode" << endl;
+}
