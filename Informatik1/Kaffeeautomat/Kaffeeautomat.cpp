7724282d41a9fa87587d3d83f50ff1a00da4527c
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-11-24 12:35:46 +0100

    B4.1.2 Umlaute werden korrekt angezeigt Umlaute in der Ausgabe wurden durch ANSI-Codes ersetzt und werden nun in der Konsole korrekt dargestellt

--- Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
+++ Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
@@ -6,11 +6,11 @@ int main()
 	char zeichen;
 
 	cout << "Herzlich Willkommen beim Kaffe-Automaten!" << endl
-		<< "Bitte wählen Sie aus:" << endl
+		<< "Bitte w\x0084hlen Sie aus:" << endl
 		<< "(k) Kaffee" << endl
 		<< "(e) Espresso" << endl
 		<< "(s) Service-Mode" << endl;
 
 	cin >> zeichen;
-	cout << "Sie haben sich für Auswahl " << zeichen << " entschieden";
+	cout << "Sie haben sich f\x0081r Auswahl " << zeichen << " entschieden";
 }
