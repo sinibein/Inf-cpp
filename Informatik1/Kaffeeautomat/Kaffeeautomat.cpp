c9c6b034055a672d1dff75f0bb1f465f49236ca6
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-11-24 12:12:59 +0100

    B4.1.2 Kaffeeautomat.cpp fertiggestellt Benutzereingabe mit 'cin' hinzugefügt Ausgabe des eingegeben Zeichens mit 'cout' hinzugefügt

--- Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
+++ Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 int main()
 {
+	char zeichen;
+
 	cout << "Herzlich Willkommen beim Kaffe-Automaten!" << endl
 		<< "Bitte wählen Sie aus:" << endl
 		<< "(k) Kaffee" << endl
 		<< "(e) Espresso" << endl
 		<< "(s) Service-Mode" << endl;
+
+	cin >> zeichen;
+	cout << "Sie haben sich für Auswahl " << zeichen << " entschieden";
 }
