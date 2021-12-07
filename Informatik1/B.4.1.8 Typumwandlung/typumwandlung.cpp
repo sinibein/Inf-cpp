655c6afca072ffb45b8d4ac8eac03f99970f8623
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-12-07 17:14:42 +0100

    B.4.1.8 Konvertierungen angepasst beim kompilieren gibt es keine Warnungen oder Fehler mehr

--- Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
+++ Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main()
@@ -6,11 +7,14 @@ int main()
 	int l;
 	double laenge{ 100.05 };
 	char zeichen;
-	l = laenge;			//Möglicher Datenverlust bei der Konvertierung vom Typ double zu int, weil double entspricht 64 Bit und int entspricht 32 Bit. Zu große Werte können nicht umgewandelt werden und Nachkommastellen gehen verloren
-	zeichen = laenge;	//Möglicher Datenverlust bei der Konvertierung vom Typ double in char, weil double entspricht 64 Bit und char entspricht 8 Bit. Werte über 255 (bzw. 127) werden nicht korrekt umgewandelt und Nachkommastellen gehen verloren
-	cout << laenge << " " << 1 << zeichen << endl;
+	l = static_cast<int>(laenge);			//Möglicher Datenverlust bei der Konvertierung vom Typ double zu int, weil double entspricht 64 Bit und int entspricht 32 Bit. Zu große Werte können nicht umgewandelt werden und Nachkommastellen gehen verloren
+	zeichen = static_cast<char>(laenge);	//Möglicher Datenverlust bei der Konvertierung vom Typ double in char, weil double entspricht 64 Bit und char entspricht 8 Bit. Werte über 255 (bzw. 127) werden nicht korrekt umgewandelt und Nachkommastellen gehen verloren
+	cout << laenge << " " << 1 << " " <<  zeichen << endl;
 	string messlatte{ "777.77" };
-	laenge = static_cast<double>(messlatte);  //Ein string kann nicht ohne weiteres in ein double konvertiert werden
+	//laenge = static_cast<double>(messlatte);  //Ein string kann nicht ohne weiteres in ein double konvertiert werden
 
+	istringstream iss;  //Steht so nicht im Skript, aber funktioniert
+	iss.str(messlatte);
+	iss >> laenge;
 	return 0;
 }
