4391b456979f1f706b28eb0ff71c476e9268de97
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-12-07 16:49:28 +0100

    B.4.1.8 Kommentare hinzugefügt

--- Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
+++ Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
@@ -6,11 +6,11 @@ int main()
 	int l;
 	double laenge{ 100.05 };
 	char zeichen;
-	l = laenge;
-	zeichen = laenge;
+	l = laenge;			//Möglicher Datenverlust bei der Konvertierung vom Typ double zu int, weil double entspricht 64 Bit und int entspricht 32 Bit. Zu große Werte können nicht umgewandelt werden und Nachkommastellen gehen verloren
+	zeichen = laenge;	//Möglicher Datenverlust bei der Konvertierung vom Typ double in char, weil double entspricht 64 Bit und char entspricht 8 Bit. Werte über 255 (bzw. 127) werden nicht korrekt umgewandelt und Nachkommastellen gehen verloren
 	cout << laenge << " " << 1 << zeichen << endl;
 	string messlatte{ "777.77" };
-	laenge = static_cast<double>(messlatte);
+	laenge = static_cast<double>(messlatte);  //Ein string kann nicht ohne weiteres in ein double konvertiert werden
 
 	return 0;
 }
