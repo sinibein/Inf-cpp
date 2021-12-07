8153e04daf2d0144b5559a737b75c00841e5278c
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-12-07 17:20:42 +0100

    B.4.1.8 (3) Kommentar zur Ausgabe hinzugefügt

--- Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
+++ Informatik1/B.4.1.8 Typumwandlung/typumwandlung.cpp
@@ -10,6 +10,14 @@ int main()
 	l = static_cast<int>(laenge);			//Möglicher Datenverlust bei der Konvertierung vom Typ double zu int, weil double entspricht 64 Bit und int entspricht 32 Bit. Zu große Werte können nicht umgewandelt werden und Nachkommastellen gehen verloren
 	zeichen = static_cast<char>(laenge);	//Möglicher Datenverlust bei der Konvertierung vom Typ double in char, weil double entspricht 64 Bit und char entspricht 8 Bit. Werte über 255 (bzw. 127) werden nicht korrekt umgewandelt und Nachkommastellen gehen verloren
 	cout << laenge << " " << 1 << " " <<  zeichen << endl;
+	/*
+	Ausgabe: 100.05 1 d
+			Zuerst wird die Variable laenge ausgegeben. Da die unkonvertierte Variable ausgegeben wird, sind Nachkommastellen vorhanden
+			Anschließend wird die Ziffer 1 ausgegeben, und dann die Variable zeichen als 'd'. Es wird ein 'd' angezeigt, da der char-Variable zeichen
+			zuvor der Wert von laenge zugewiesen wurde. Dabei gingen die Nachkommastellen verloren und es wurde die Zahl 100 abgespeichert. In der ASCII-Tabelle steht
+			die 100 für 'd', daher wird dieses Zeichen ausgegeben
+	*/
+
 	string messlatte{ "777.77" };
 	//laenge = static_cast<double>(messlatte);  //Ein string kann nicht ohne weiteres in ein double konvertiert werden
 
