bf0067a9cf925f243c2d843002eb3011044c6b69
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2021-12-08 13:11:38 +0100

    B.4.5 Menü für Kaffee erstellt
    Bei der Auswahl von Kaffee wird nun nach Milch und Zucker gefragt und der Preis berechnet

--- Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
+++ Informatik1/Kaffeeautomat/Kaffeeautomat.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-	int kaffeebohnen = 1000;
-	int espressobohnen = 1000;
-	int wasser = 5000;
-	int milch = 1000;
-	int zucker = 500;
+	int vorrat_kaffeebohnen = 1000;
+	int vorrat_espressobohnen = 1000;
+	int vorrat_wasser = 5000;
+	int vorrat_milch = 1000;
+	int vorrat_zucker = 500;
 
 	int pulvermenge = 5;
 	int wasser_kaffee = 125;
@@ -15,7 +15,14 @@ int main()
 	int zucker = 3;
 	int milch = 30;
 
+	double preis_tasse = 1.0;
+	double aufpreis = 0.1;
+
 	cout << "Herzlich Willkommen beim Kaffe-Automaten!" << endl
+		<< "Preis pro Tasse:" << endl
+		<< "Kaffee oder Espresso: " << preis_tasse << " Euro" << endl
+		<< "Milch oder Zucker:  " << aufpreis << " Euro" << endl
+		<< endl 
 		<< "Bitte w\x0084hlen Sie aus:" << endl
 		<< "(k) Kaffee" << endl
 		<< "(e) Espresso" << endl
@@ -26,7 +33,32 @@ int main()
 	std::cin >> zeichen;
 
 	if (zeichen == 'k') {
-		cout << "Sie haben sich f\x81r die Auswahl Kaffee entschieden.";
+		char auswahl_zucker, auswahl_milch;
+		
+		cout << "Sie haben sich f\x81r die Auswahl Kaffee entschieden." << endl
+			<< "M\x94 \bchten sie Zucker (j\\n)? ";
+		cin >> auswahl_zucker;
+
+		cout << "M\x94 \bchten sie Milch (j\\n)? ";
+		cin >> auswahl_milch;
+
+		if ( !(auswahl_milch == 'j' || auswahl_milch == 'n') || !(auswahl_zucker == 'j' || auswahl_zucker == 'n'))
+		{
+			cout << "Falsche Eingabe!" << endl;
+			system("pause");
+			return 0;
+		}
+
+		double preis = preis_tasse;
+		if (auswahl_milch == 'j')
+		{
+			preis += aufpreis;
+		}
+		if (auswahl_zucker == 'j')
+		{
+			preis += aufpreis;
+		}
+		cout << "Bitte " << preis << " Euro eingeben und ENTER druecken: " << endl;
 	}
 
 	else if (zeichen == 'e') {
