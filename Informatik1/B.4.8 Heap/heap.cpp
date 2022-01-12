cc7400ed505a96261959f7396cc33051ed793f85
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2022-01-12 12:27:17 +0100

    B.4.9.3 Berechnung von Mittelwert und Varianz in Funktion ausgelagert

--- Informatik1/B.4.8 Heap/heap.cpp
+++ Informatik1/B.4.8 Heap/heap.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
+void mittelwert_varianz(double* messwerte, int anzahl_messwerte, double& r_mittelwert, double& r_varianz);
+
 int main()
 {
 	cout << "Wie viele Messwerte m\x94 \bchten Sie eingeben? ";
 	int anzahl_messwerte;
 	cin >> anzahl_messwerte;
 
-	double *messwerte = new double[anzahl_messwerte];
+	double* messwerte = new double[anzahl_messwerte];
 	for (int i = 0; i < anzahl_messwerte; i++)
 	{
 		cout << "Messwert Nr." << i + 1 << " eingeben: ";
 		cin >> messwerte[i];
 	}
 
-	double summe = 0;
-	for (int i = 0; i < anzahl_messwerte; i++)
-	{
-		summe += messwerte[i];
-	}
-	double mittelwert = summe / anzahl_messwerte;
-	
-	double s = 0;
-	for (int i = 0; i < anzahl_messwerte; i++)
-	{
-		s += pow(messwerte[i] - mittelwert, 2);
-	}
-	double varianz = s / (anzahl_messwerte - 1);
-
+	double mittelwert, varianz;
+	mittelwert_varianz(messwerte, anzahl_messwerte, mittelwert, varianz);
 
 	cout << "----------------------" << endl
 		<< "Messwerte:" << endl;
@@ -39,7 +30,26 @@ int main()
 		<< "Mittelwert: " << mittelwert<< endl
 		<< "Varianz: " << varianz << endl;
 
+
 	delete[] messwerte;
 
 	return 0;
+}
+
+void mittelwert_varianz(double* messwerte, int anzahl_messwerte, double& r_mittelwert, double& r_varianz)
+{
+	double summe = 0;
+	for (int i = 0; i < anzahl_messwerte; i++)
+	{
+		summe += messwerte[i];
+	}
+	r_mittelwert = summe / anzahl_messwerte;
+
+	double s = 0;
+	for (int i = 0; i < anzahl_messwerte; i++)
+	{
+		s += pow(messwerte[i] - r_mittelwert, 2);
+	}
+	r_varianz = s / (anzahl_messwerte - 1);
+
 }
