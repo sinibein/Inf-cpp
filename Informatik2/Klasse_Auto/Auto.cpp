9cfb40919a5601537393ccc801bb13969f9ecce2
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2022-04-25 14:35:46 +0200

    C.4.12.1: 4) Konstruktoren in Auto.cpp verschoben

--- Informatik2/Klasse_Auto/Auto.cpp
+++ Informatik2/Klasse_Auto/Auto.cpp
@@ -1,1 +1,33 @@
 #include "Auto.h"
+
+//Standardkonstruktor
+Auto::Auto()
+{
+	cout << "Standardkonstruktor aufgerufen" << endl;
+	radzahl = 0;
+	farbe = "";
+	ps = 0.0;
+	anzahl_sitze = 0;
+	spritverbrauch = 0.0;
+	tankinhalt = 0.0;
+	tankgroesse = 0.0;
+}
+
+//Allgemeiner Konstruktor
+Auto::Auto(int radzahl, string farbe, float ps, int anzahl_sitze, float spritverbrauch, float tankinhalt, float tankgroesse)
+	:radzahl(radzahl), farbe(farbe), ps(ps), anzahl_sitze(anzahl_sitze), spritverbrauch(spritverbrauch), tankinhalt(tankgroesse), tankgroesse(tankgroesse)
+{
+	cout << "Allgemeiner Konstruktor aufgerufen" << endl;
+};
+
+//Kopierkonstruktor
+Auto::Auto(const Auto& a) :radzahl(a.radzahl), farbe(a.farbe), ps(a.ps), anzahl_sitze(a.anzahl_sitze), spritverbrauch(a.spritverbrauch), tankinhalt(a.tankgroesse), tankgroesse(a.tankgroesse)
+{
+	cout << "Kopierkonstruktor aufgerufen" << endl;
+}
+
+//Destruktor
+Auto::~Auto()
+{
+	cout << "Destruktor aufgerufen" << endl;
+}
--- Informatik2/Klasse_Auto/Auto.h
+++ Informatik2/Klasse_Auto/Auto.h
@@ -17,35 +17,15 @@ private:
 
 public:
 	//Standardkonstruktor
-	Auto()
-	{
-		cout << "Standardkonstruktor aufgerufen" << endl;
-		radzahl = 0;
-		farbe = "";
-		ps = 0.0;
-		anzahl_sitze = 0;
-		spritverbrauch = 0.0;
-		tankinhalt = 0.0;
-		tankgroesse = 0.0;
-	}
+	Auto();
 
 	//allgemeiner Konstruktor
-	Auto(int radzahl, string farbe, float ps, int anzahl_sitze, float spritverbrauch, float tankinhalt, float tankgroesse)
-		:radzahl(radzahl), farbe(farbe), ps(ps), anzahl_sitze(anzahl_sitze), spritverbrauch(spritverbrauch), tankinhalt(tankgroesse), tankgroesse(tankgroesse)
-	{
-		cout << "Allgemeiner Konstruktor aufgerufen" << endl;
-	};
+	Auto(int radzahl, string farbe, float ps, int anzahl_sitze, float spritverbrauch, float tankinhalt, float tankgroesse);
 
 	//Kopierkonstruktor
-	Auto(const Auto& a) :radzahl(a.radzahl), farbe(a.farbe), ps(a.ps), anzahl_sitze(a.anzahl_sitze), spritverbrauch(a.spritverbrauch), tankinhalt(a.tankgroesse), tankgroesse(a.tankgroesse)
-	{
-		cout << "Kopierkonstruktor aufgerufen" << endl;
-	}
+	Auto(const Auto& a);
 
 	//Destruktor
-	~Auto()
-	{
-		cout << "Destruktor aufgerufen" << endl;
-	}
+	~Auto();
 };
 
