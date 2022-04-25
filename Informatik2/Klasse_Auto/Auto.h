505dea9d1f771b04f7756b1384bbc108af3a72e5
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2022-04-25 13:03:15 +0200

    C.4.12.1: 3) Konstruktoren erstellt
    Standardkonstruktor, allgemeinen Konstruktor, Kopierkonstruktor und Destruktor für die Klasse "Auto" in der Headerdatei erstellt

--- Informatik2/Klasse_Auto/Auto.h
+++ Informatik2/Klasse_Auto/Auto.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -15,5 +16,36 @@ private:
 	float tankgroesse;
 
 public:
+	//Standardkonstruktor
+	Auto()
+	{
+		cout << "Standardkonstruktor aufgerufen" << endl;
+		radzahl = 0;
+		farbe = "";
+		ps = 0.0;
+		anzahl_sitze = 0;
+		spritverbrauch = 0.0;
+		tankinhalt = 0.0;
+		tankgroesse = 0.0;
+	}
+
+	//allgemeiner Konstruktor
+	Auto(int radzahl, string farbe, float ps, int anzahl_sitze, float spritverbrauch, float tankinhalt, float tankgroesse)
+		:radzahl(radzahl), farbe(farbe), ps(ps), anzahl_sitze(anzahl_sitze), spritverbrauch(spritverbrauch), tankinhalt(tankgroesse), tankgroesse(tankgroesse)
+	{
+		cout << "Allgemeiner Konstruktor aufgerufen" << endl;
+	};
+
+	//Kopierkonstruktor
+	Auto(const Auto& a) :radzahl(a.radzahl), farbe(a.farbe), ps(a.ps), anzahl_sitze(a.anzahl_sitze), spritverbrauch(a.spritverbrauch), tankinhalt(a.tankgroesse), tankgroesse(a.tankgroesse)
+	{
+		cout << "Kopierkonstruktor aufgerufen" << endl;
+	}
+
+	//Destruktor
+	~Auto()
+	{
+		cout << "Destruktor aufgerufen" << endl;
+	}
 };
 
