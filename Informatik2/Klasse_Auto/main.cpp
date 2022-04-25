532fa908f3ea74dfb08e665e92659a4c3bb770d7
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2022-04-25 12:45:54 +0200

    C.4.12.1: 2) Testprogramm in main.cpp geschrieben
    Objektinstanziierung um Datensegment, auf dem Stack, auf dem Heap

--- Informatik2/Klasse_Auto/main.cpp
+++ Informatik2/Klasse_Auto/main.cpp
@@ -1,4 +1,15 @@
+#include "Auto.h"
+
+//Objekt im Datensegment erstellen
+Auto a;
+
 int main()
 {
-
+	//Objekt auf dem Stack erstellen
+	Auto b;
+	
+	//Objekt auf dem Heap erstellen
+	Auto* c = new Auto();
+	//Objekt vom Heap löschen
+	delete c;
 }
