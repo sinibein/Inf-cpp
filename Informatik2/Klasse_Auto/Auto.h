ca6c4286f0f15629f15f7b854d44562f14fefd5e
Nils Beinhorn <nils.beinhorn@stud.hawk.de>
2022-04-25 12:33:26 +0200

    C.4.12.1: 1) Klasse "Auto" erstellt, private Attribute hinzugefügt

---
+++ Informatik2/Klasse_Auto/Auto.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Auto
+{
+private:
+	int radzahl;
+	string farbe;
+	float ps;
+	int anzahl_sitze;
+	float spritverbrauch;
+	float tankinhalt;
+	float tankgroesse;
+
+public:
+};
+
